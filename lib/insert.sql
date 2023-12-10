8180a5372828496490e5da30f10b274cdb2e5d2a
mosesmramba <moses.mramba@student.moringaschool.com>
2023-12-10 15:03:35 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 8, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 1, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 9, "M", "Tan", "Slimy", 0);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "White", "naive", 1);
+INSERT INTO bears VALUES (8,null, 15, "M", "black", "aggressive", 0);
